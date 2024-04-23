4c428a9c505faa7b7247535cccc512975f2b42c1
Matt Nappo <mattnappo@gmail.com>
2024-04-23 12:05:57 -0400

    Added type replacement

--- car/examples/references.rs
+++ car/examples/references.rs
@@ -1,9 +1,10 @@
-fn a() {
+fn a(p: (i128, i128)) {
     // b();
 }
 
-fn b() {
-    a();
+fn b(i: i32) {
+    a((2, 2));
+    let x: f32 = 3.1;
 }
 
 struct Unit;
--- car/src/lib.rs
+++ car/src/lib.rs
@@ -1,12 +1,27 @@
+#[macro_use]
+extern crate quote;
+
 use anyhow::Result;
+use proc_macro2::Span;
+use sha1::{Digest, Sha1};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use syn;
+use syn::visit_mut::VisitMut;
 
-use sha1::{Digest, Sha1};
+macro_rules! ident {
+    ($hash:expr) => {
+        syn::Ident::new(&format!("__refr__{}", $hash), Span::call_site())
+    };
+}
 
-fn hash<T: std::fmt::Debug>(x: T) -> String {
+fn write_ast(file: &str, ast: syn::File) -> Result<()> {
+    std::fs::write(file, format!("{}", quote!(#ast)))?;
+    Ok(())
+}
+
+fn hash<T: std::fmt::Debug>(x: &T) -> String {
     let mut hasher = Sha1::new();
     hasher.update(format!("{x:?}").as_bytes());
     format!("{:x}", hasher.finalize())
@@ -39,13 +54,14 @@ impl Compiler {
         let mut content = String::new();
         fd.read_to_string(&mut content)?;
 
-        let file = syn::parse_file(&content)?;
+        let mut file = syn::parse_file(&content)?;
 
         println!("AST:\n{:#?}", file);
 
         // Extract things that can be hashed
         let items = file
             .items
+            .clone()
             .into_iter()
             .filter(|item| match item {
                 syn::Item::Fn(_)
@@ -68,6 +84,12 @@ impl Compiler {
          *     requires me to handle impl blocks first
          */
 
+        // SO DO THE MUT-TRAVERSAL HERE
+        let mut ll = Linker::default();
+        ll.visit_file_mut(&mut file);
+
+        println!("AST AFTER LINKING:\n{:#?}", file);
+
         // Identifier --> Hash
         let names_to_h = items
             .iter()
@@ -77,13 +99,58 @@ impl Compiler {
         // Hash --> AST
         let h_to_ast = items // fix item.clone to be the ident
             .into_iter()
-            .map(|item| (hash(item.clone()), item))
+            .map(|item| (hash(&item), item))
             .collect::<HashMap<String, syn::Item>>();
+
+        write_ast("output.rs", file)?; // For debugging
 
         Ok(Compiler {
             names_to_h,
             h_to_ast,
         })
+    }
+}
+
+/// Change all function calls, type refs, etc... to their hashes
+/// Hashes AND links at the same time
+#[derive(Default)]
+struct Linker {
+    names_to_h: HashMap<String, String>,
+    // h_to_ast: HashMap<String, syn::Item>,
+}
+
+impl VisitMut for Linker {
+    // Visit a function: replace all custom types in arguments with hashes
+    // Each fn will return its hash
+    // potentially change to `visit_signature_mut` ?
+    // or visit_type_mut?
+    /*
+    fn visit_item_fn_mut(&mut self, node: &mut syn::ItemFn) {
+        node.sig.inputs.iter_mut().for_each(|a| match a {
+            syn::FnArg::Typed(arg) => {
+                let h = hash(&*arg.ty);
+                match *arg.ty {
+                        Path(TypePath),
+                        Reference(TypeReference),
+                        Slice(TypeSlice),
+                        TraitObject(TypeTraitObject),
+                        Tuple(TypeTuple),
+                        Verbatim(TokenStream),
+                }
+            }
+            syn::FnArg::Receiver(arg) => {}
+        })
+    }
+    */
+
+    fn visit_type_path_mut(&mut self, node: &mut syn::TypePath) {
+        let h = hash(&node.path);
+        let mut s = syn::punctuated::Punctuated::new();
+        s.push_value(syn::PathSegment {
+            ident: ident!(h),
+            arguments: syn::PathArguments::None,
+        });
+        node.path.segments = s;
     }
 }
 
